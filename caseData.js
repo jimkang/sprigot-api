2010f864466848d649d65c6c812dff8d6759826a
jimkang <jimkang@gmail.com>
2013-07-16 23:13:41 -0400

    Added ids to data.

--- caseData.js
+++ caseData.js
@@ -1,20 +1,25 @@
 var caseDataSource = {
+  id: 'discovery',
   name: 'Discovery',
   text: 'Last September, I discovered that a friend of mine, \'A\', had been arrested.',
   children: [
     {
+      id: 'thecase',
       name: 'The Case',
       text: 'He was charged with stuff. They had a tip about plant growing equipment in a derelict storage space.',
       children: [
         {
+          id: 'charges',
           name: 'Charges',
           text: 'He was charged with the manufacture of a controlled substance.',
         },
         {
+          id: 'feds',
           name: 'The Feds',
           text: 'The feds watched him for nine months.',
           children: [
             {
+              id: 'money',
               name: 'Money',
               text: 'How much did that cost?',
               children: [
