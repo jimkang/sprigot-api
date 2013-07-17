5618b0be48c85b1f9a8aa63149df47d9cc792d28
jimkang <jimkang@gmail.com>
2013-07-16 21:53:04 -0400

    Cut out rects from data, added more nodes for testing.

--- caseData.js
+++ caseData.js
@@ -1,51 +1,30 @@
 var caseDataSource = {
-  name: 'root',
+  name: 'Discovery',
   text: 'Last September, I discovered that a friend of mine, \'A\', had been arrested.',
-  rect: {
-    x: 50,
-    y: 10,
-    width: 300,
-    height: 300,
-    fill: 'gray'
-  },
   children: [
     {
-      name: 'theCase',
+      name: 'The Case',
       text: 'He was charged with stuff. They had a tip about plant growing equipment in a derelict storage space.',
-      rect: {
-        x: 70,
-        y: 400,
-        width: 300,
-        height: 300,
-        fill: 'green'
-      },
       children: [
         {
-          name: 'charges',
+          name: 'Charges',
           text: 'He was charged with the manufacture of a controlled substance.',
-          rect: {
-            x: 440,
-            y: 800,
-            width: 300,
-            height: 300,
-            fill: 'red'
-            },
-            group: {
-              id: 'charges'
-            },
-            outbound: [
-            ]
         },
         {
-          name: 'feds',
+          name: 'The Feds',
           text: 'The feds watched him for nine months.',
-          rect: {
-            x: 450,
-            y: 400,
-            width: 300,
-            height: 300,
-            fill: 'red',
-          }
+          children: [
+            {
+              name: 'Money',
+              text: 'How much did that cost?',
+              children: [
+                {
+                  name: 'What else could have been done with that money?',
+                  text: 'Feed four families for a year?'
+                }
+              ]
+            }
+          ]
         }
       ]
     }
