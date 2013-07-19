2c7742c7818bb187d9cd871d0cd59dda53e47bd9
jimkang <jimkang@gmail.com>
2013-07-19 01:32:30 -0400

    More content.

--- caseData.js
+++ caseData.js
@@ -39,6 +39,59 @@ var caseDataSource = {
                   body: "<p>According to the affidavit, the DEA started the case about two years before the arrest. The manager of a storage facility (which I looked up and appears to be sheds behind a private home) busted into storage units because of unpaid rent and found a bunch of indoor marijuana growing equipment - grow lamps, plastic swimming pools, pots, etc.</p><p>They traced the renters to a business, and one of the owners of that business had a Facebook account. That Facebook account had pictures of the guy with pot plants.</p><p>They also traced the license plate of a van that came to pick the equipment up a few days later to someone we'll call Z, Sr. No connection to N, at this point.</p>",
                   children: [
                     {
+                      id: "concernedcitizen",
+                      title: "Concerned citizen",
+                      body: "A year and a half after the storage facility incident, a 'concerned citizen' called to complain about one of his neighbors, who happened to be N. He complained that N's house smelled weird. His other complaints were that N was never outside, and no lights were ever on inside his house, except for possibly some screens.</p><p>They had not connected this complaint to the storage locker, yet, <i>this complaint was enough</i> for them to start investigating the property.",
+                      children: [
+                        {
+                          id: "concerned2",
+                          title: "Concerns",
+                          body: "There was a neighbor quoted in the local papers several times who I would bet is Concerned Citizen. On record, Concerned Citizen complained about N not mowing his lawn quickly enough, his home being too dark, and that he never saw N use his grill. He sounded like he relished this situation."
+                        },
+                        {
+                          id: "garbage",
+                          title: "Garbage",
+                          body: "<p>Law enforcement started by looking through property records and his garbage and mail, which I guess is legal, as long as they don't open the mail. Through the mail, they were able to connect N to a person we'll call, Z, Jr., who is related to the aforementioned Z, Sr.</p><p>Then, they started <i>three months</i> of surveillance. Eventually, they got N's electric bills which were high and characteristic of the cost of operating lots of indoor growing lamps.</p><p>That, along with another chat with Concerned Citizen, plus the connections to the Zs, got them their warrant.</p>",
+                          children: [
+                            {
+                              id: "nothingtohide",
+                              title: "Nothing to hide?",
+                              body: "<p>Let's consider for a moment, the implications of the police digging through the garbage and mail of everyone that is reported to be weird and unlike the other people in the neighborhood. Let's say you think that marijuana is the scourge of mankind, even. Supposing that, they struck gold in this case.</p><p>But what if there were something about you that caught the eye of your neighborhood busybody? Would it be fair for the police and some agency to start digging through your garbage because someone made a specious complaint about you? If so, should we then all live our lives as though someone might start going through our garbage and mail?</p><p>If your answer is \"I have nothing to hide\", maybe you should start with telling me where you live, and we can see if you have nothing to hide?</p><p>(I'm mostly just kidding, as I don't have time to provide you with personal Stasi service, but I hope that for a moment, you thought I wasn't.)</p>",
+                            },
+                            {
+                              id: "nstate",
+                              title: "Unknowns about N",
+                              body: "I knew that N was in a jail. However, I didn't know if he was safe. I didn't know if he had a lawyer other than the public defender mentioned in news of his arraignment. I didn't know if he was in contact with his family, which I had heard in passing had become more conservative over the years. I didn't know how to contact him.",
+                              children: [
+                                {
+                                  id: "otherguys",
+                                  title: "The other guys",
+                                  body: "<p>What happened to the other guys named in the affidavit? Presumably, they were his associates. What was their role? Did N do anything other than live in the house with the plants? Why was N the only one arrested? Did they have an arrangement that unfairly exposed him to risk?</p><p>C and I speculated, but we never heard anything about them again.</p>"
+                                },
+                                {
+                                  id: "contact",
+                                  title: "Old-fashioned letter writing",
+                                  body: "<p>Being a federal detainee, he was not listed on the jail's web site. I had to call to verify that he was there and that I could write letters to him like the web site said you could do with regular prisoners. (The jail workers were surly, in case you were wondering.)</p><p>I very carefully wrote a letter expressing support and holding back advice-giving and curiosity. I ran it by my wife, who pointed out it was not the right time to make jokes about a \"So you've been arrested\" pamphlet.</p><p>The rights of jailed did not seem all that robust, so I worried about whether some detail about the letter would get it rejected by the jail mail readers. I wanted as much accountability as possible, so this is how I became a regular user of Priority Mail.</p>",
+                                  children: [
+                                    {
+                                      id: "handwriting",
+                                      title: "Hand-writing",
+                                      body: "My printer was out of ink, and I had planned to toss it out and never have a printer again. So, I wrote that letter with a pen on paper. Having been over a decade since I last did that, the process of manual writing hurt my hand."
+                                    },
+                                    {
+                                      id: "transcript",
+                                      title: "Transcripts",
+                                      body: "<p>While I was waiting for a reply and wondering if any would come at all, I felt information-starved. So, I tried to fish out more details by getting the transcript of his arraignment.</p><p>Unlike some kind of modern service, courts don't just post their transcripts on their web site. Instead, there's a little cottage industry of stenographers that go to hearings, transcribes them, then sells them for $100, if you're the first buyer. $30 if you're the second or later buyers</p><p>I seesawed on it, and ultimately decided to skip it, as I guessed that very little additional detail was likely to spring from an arraignment.</p>"
+                                    }
+                                  ]
+                                }
+                              ]
+                            }
+                          ]
+                        }
+                      ]
+                    },
+                    {
                       id: "wall",
                       title: "Someone's wall",
                       body: "<p>There's a lot that bothers me about this Facebook connection. First, how incredibly stupid of this idiotic fool to post pictures of himself with pot plants, knowing what he's got in his storage unit, to Facebook. Even if you limit access to immediate friends, what's to stop them from talking? And if you are not concerned with your own freedom, what about that of your associates?</p><p>More importantly, how did law enforcement get into his Facebook pictures? I think it's possible, but highly unlikely that those photos were completely public. Did they get a subpoena? There's no mention of it in the affidavit, which you'd think there'd be.</p>",
@@ -54,26 +107,7 @@ var caseDataSource = {
                           body: "<p>I was disgusted by Facebook's role in this, both as a tool and as a company. I never want to accidentally end up a link in a chain that fucks up good people for no reason. So, I decided to nuke all of my content there. I had already stopped posting to it a little while ago, but I had left my stuff up.</p><p>I know it's likely that nothing is truly deleted there – just marked as deleted. Still, I want to make it hard as I can for entities tapping into Facebook and for Facebook itself to exploit any information I may have offered.</p><p>I wrote a set of Python scripts that used their Graph API to delete whatever I could. Their API is wonky, but I persisted until I got it. I know it wasn't entirely rational, and not a platonically optimal use of my time, but it felt right.</p>"
                         }
                       ]
-                    },
-                    {
-                      id: "concernedcitizen",
-                      title: "Concerned citizen",
-                      body: "A year and a half after the storage facility incident, a 'concerned citizen' called to complain about one of his neighbors, who happened to be N. He complained that N's house smelled weird. His other complaints were that N was never outside, and no lights were ever on inside his house, except for possibly some screens.</p><p>They had not connected this complaint to the storage locker, yet, <i>this complaint was enough</i> for them to start investigating the property.",
-                      children: [
-                        {
-                          id: "garbageandmail",
-                          title: "Garbage, mail, and bills",
-                          body: "<p>Law enforcement started by looking through property records and his garbage and mail, which I guess is legal, as long as they don't open the mail. Through the mail, they were able to connect N to a person we'll call, Z, Jr., who is related to the aforementioned Z, Sr.</p><p>Then, they started <i>three months</i> of surveillance. Eventually, they got N's electric bills which were high and characteristic of the cost of operating lots of indoor growing lamps.</p><p>That, along with another chat with Concerned Citizen, plus the connections to the Zs, got them their warrant.</p>",
-                          children: [
-                            {
-                              id: "nothingtohide",
-                              title: "Nothing to hide?",
-                              body: "<p>Let's consider for a moment, the implications of the police digging through the garbage and mail of everyone that is reported to be weird and unlike the other people in the neighborhood. Let's say you think that marijuana is the scourge of mankind, even. Supposing that, they struck gold in this case.</p><p>But what if there were something about you that caught the eye of your neighborhood busybody? Would it be fair for the police and some agency to start digging through your garbage because someone made a specious complaint about you? If so, should we then all live our lives as though someone might start going through our garbage and mail?</p><p>If your answer is \"I have nothing to hide\", maybe you should start with telling me where you live, and we can see if you have nothing to hide?</p><p>(I'm mostly just kidding, as I don't have time to provide you with personal Stasi service, but I hope that for a moment, you thought I wasn't.)</p>",
-                            }
-                          ]
-                        }
-                      ]
-                    }
+                    }                    
                   ]
                 }                
               ]
