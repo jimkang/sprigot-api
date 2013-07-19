faee02406aac38e393a9e7d84e4bde7dd3e2ebf7
jimkang <jimkang@gmail.com>
2013-07-18 21:22:24 -0400

    Content update.

--- caseData.js
+++ caseData.js
@@ -1,6 +1,6 @@
 var caseDataSource = {
   id: "notonline",
-  title: "Not online.",
+  title: "Not online",
   body: "<p>A friend of mine, 'N', had not been online in quite some time. He hadn't responded to texts or calls, either. I assumed that he had gone on vacation to some place where his phone was no good.</p><p>At some point, I saw in Messages that our last chat had been almost a month ago.</p>",
   children: [
     {
@@ -16,17 +16,22 @@ var caseDataSource = {
         {
           id: "localnews",
           title: "Local news web sites",
-          body: "There were several hits on local news web sites, and web sites that aggregated and copied content from local news web sites. He had been arrested by the DEA and was being held in jail.",
+          body: "<p>There were several hits on local news web sites, as well as web sites that aggregated and copied content from local news web sites. They all said the same thing, but I clicked on all of them.</p><p>He had been arrested by the DEA and was being held in jail for the manufacture of marijuana with the intent to distribute.</p><p>They had pictures of him being lead from his home by law enforcement. My friend C found video footage of N being walked out and big weed plants being brought out of the house, trophy-style. It was surreal.</p>",
           children: [
             {
-              id: "thecase",
-              title: "The Case",
-              body: "He was charged with stuff. They had a tip about plant growing equipment in a derelict storage space.",
+              id: "dopeonthetable",
+              title: "Dope on the table",
+              body: "<p>Since crappy local TV news was there, no doubt they were invited by the DEA or local police. They said they pulled out over 500 plants, and articles gave an astronomical 'street value,' which I later learned were inflated for drama.</p>"
+            },
+            {
+              id: "charge",
+              title: "The charge",
+              body: "<p>Most of the reporting was simply printing whatever the police wanted to say, as well as reaction quotes from the neighbors. Not incredibly useful. However, they did post the criminal complaint, which had a wealth of reliable information.</p><p>N's charged was Manufacturing a Schedule I Controlled Substance, in violation of Title 21 United States Code, Sections 841(a)(1) and 841(b)(l)(B). It says that if the violation involves<blockquote>100 kilograms or more of a mixture or substance containing a detectable amount of marihuana, or 100 or more marihuana plants regardless of weight</blockquote><blockquote>...such person shall be sentenced to a term of imprisonment which may not be less than 5 years and not more than 40 years...</blockquote>The local media had reported an estimated 500 plants, so he was likely well above 100. An affidavit was attached to the complaint. It went through the smooth moves the DEA conducted to build a case for a warrant.</p>",
               children: [
                 {
-                  id: "charges",
-                  title: "Charges",
-                  body: "He was charged with the manufacture of a controlled substance.",
+                  id: "investigation",
+                  title: "Investigation",
+                  body: "According to the criminal complaint, ",
                 },
                 {
                   id: "feds",
@@ -47,7 +52,12 @@ var caseDataSource = {
                   ]
                 }
               ]
-            }
+            },
+            {
+              id: "comments",
+              title: "Read the comments?",
+              body: "<p>Surprisingly, 100% of the comments were not of the \"these deadbeat criminals are ruining our country THANKS OBAMA\" nature. Instead, in various levels of articulation, they all questioned the cost and value of arrests, raids, and investigations like this.</p><p>When I went back to the search results, I found that legalization advocacy web sites regularly spider out and link to these stories, pouring a payload of pro-weed people onto local news sites. So, like the racist and misogynist comments you may see on any other article, these can't be counted as reprsentative of the people.</p><p>However, months later, I read about Josh Rosenthal's (the quasi-famous mixed martial arts referee) similar arrest for growing marijuana. The comments on MMA web sites, which are not known for being particularly thoughtful or liberal, were also largely questioning the point of such federal efforts.</p>"
+            }            
           ]
         }
       ]
