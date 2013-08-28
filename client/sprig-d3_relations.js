5c7ad9d2dbd163e8800c1e2bcdbe68ec4abfa472
jimkang <jimkang@gmail.com>
2013-08-27 23:59:55 -0400

    Added mapPathToSprigInD3Tree().

--- client/sprig-d3_relations.js
+++ client/sprig-d3_relations.js
@@ -44,6 +44,72 @@ function sanitizeTreeForD3(tree) {
   return tree;
 }
 
+function mapPathToSprigInD3Tree(targetSprigId, sprigTree, depthLimit) {
+  if (targetSprigId === sprigTree.id) {
+    return [sprigTree.id];
+  }
+
+  var path = [];
+  var parentMap = {};
+  var targetSprig = null;
+
+  var currentDepth = 0;
+  var sprigsAtNextDepth = null;
+  var sprigsAtDepth = [sprigTree];
+
+  while (currentDepth <= depthLimit) {
+    sprigsAtNextDepth = [];
+
+    for (var i = 0; i < sprigsAtDepth.length; ++i) {
+      var sprig = sprigsAtDepth[i];
+
+      if (sprig.id === targetSprigId) {
+        targetSprig = sprig;
+        break;
+      }
+
+      if (currentDepth + 1 <= depthLimit && sprig) {
+        var theChildren = [];
+        if (typeof sprig.children === 'object' && sprig.children) {
+          theChildren = sprig.children;
+        }
+        else if (typeof sprig._children === 'object' && sprig._children) {
+          theChildren = sprig._children;
+        }
+
+        theChildren.forEach(function mapParent(child) {
+          parentMap[child.id] = sprig;
+        });
+
+        sprigsAtNextDepth = sprigsAtNextDepth.concat(theChildren);
+      }
+    }
+
+    if (targetSprig) {
+      break;
+    }
+
+    currentDepth++;
+    sprigsAtDepth = sprigsAtNextDepth;
+  }
+
+  if (targetSprig) {
+    var sprig = targetSprig;
+
+    while (sprig) {
+      path.unshift(sprig);
+      if (sprig.id in parentMap) {
+        sprig = parentMap[sprig.id];
+      }
+      else {
+        sprig = null;
+      }
+    }
+  }
+
+  return path;
+}
+
 module.exports = {
   serializeTreedNode: serializeTreedNode,
   sanitizeTreeForD3: sanitizeTreeForD3
