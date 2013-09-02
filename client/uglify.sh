f51c16164807fb71ba086bb510aae6c71207116b
jimkang <jimkang@gmail.com>
2013-09-02 14:30:11 -0400

    Updated uglify command to take order into account.

--- client/uglify.sh
+++ client/uglify.sh
@@ -1,1 +1,16 @@
-uglifyjs *.js -c -m -o ~/gcw/gcwsite/sprigot/sprigotclient.js
+uglifyjs \
+  camera.js \
+  sprig-d3_relations.js \
+  uid.js \
+  network.js \
+  okcanceldialog.js \
+  store.js \
+  treerenderer.js \
+  treenav.js \
+  graph.js \
+  textstuff.js \
+  divider.js \
+  historian.js \
+  sprigot.js \
+  direct.js \
+ -c -m -o ~/gcw/gcwsite/sprigot/sprigotclient.js
