50d14922eec18c85f34c573f94a15b0ba1cfa58e
jimkang <jimkang@gmail.com>
2013-11-20 20:15:40 -0500

    Updated build.sh to copy all css to the build.

--- client/build.sh
+++ client/build.sh
@@ -2,8 +2,6 @@
 cd build_stuff
 ./swaptagsforprod.sh $1
 cd ..
-#cp sprig.css ~/gcw/gcwsite/sprigot/
-cp sprig.css ~/gcw/$1/
-cp glog.css ~/gcw/$1/
+cp *.css ~/gcw/$1/
 cp lib/underscore-min.js ~/gcw/$1/
 cp lib/d3.v3.min.js ~/gcw/$1/
