48a337035b37c8730561a8d77aafadb74e5e49bc
jimkang <jimkang@gmail.com>
2013-12-20 15:37:17 -0500

    Upped the amount of entries per scroll for now.

--- client/textstuff.js
+++ client/textstuff.js
@@ -21,8 +21,8 @@ var TextStuff = {
   showGraphLink: null,
   downLink: null,
   OKCancelDialog: null,
-  editAvailable: true
-  // editAvailable: false
+  // editAvailable: true
+  editAvailable: false
 };
 
 TextStuff.init = function init(sprigotSel, graph, treeRenderer, store, 
